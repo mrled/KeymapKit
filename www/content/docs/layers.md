0218b5308976a3a00d930baabf332619db153931
Micah R Ledbetter <me@micahrl.com>
2025-07-08 07:35:40 -0500

    Add layers page and a layertest layout

---
+++ www/content/docs/layers.md
@@ -0,0 +1,44 @@
+---
+layout: site.njk
+title: Layers
+eleventyNavigation:
+  title: Layers
+  parent: docs
+  order: 30
+---
+
+KeymapKit supports multiple layers in keyboard layouts, allowing you to define different key mappings that can be switched between. This is useful for creating layouts with function layers, number layers, or any other key groupings.
+
+## Layer Entry Keys
+
+Layer entry keys are special keys that can switch between different layers. These keys are highlighted in purple to indicate their function and help users understand which keys can be used to access different layers.
+
+To define layer entry keys, use the `layerEntryKeys` parameter when creating a layer:
+
+```javascript
+KeymapLayer.fromKeyList({
+  displayName: "Second Layer",
+  shortName: "Second",
+  welcome: ["Welcome to the second layer"],
+  layerEntryKeys: ["key-id-1"], // These keys will be highlighted in purple
+  keys: [
+    // ... key definitions
+  ],
+})
+```
+
+## Example: Layer Entry Keys
+
+Here's an example layout that demonstrates layer entry key functionality:
+
+<div id="layer-test-container"></div>
+
+<script type="module">
+  import { KeymapTitleScreenLayoutLayerTest } from "/KeymapKit/keymaps/title-screen-layout-layertest.js";
+  const container = document.getElementById("layer-test-container");
+  const keymapUi = document.createElement("keymap-ui");
+  keymapUi.setModelsAndMaps([KeymapTitleScreenLayoutLayerTest]);
+  container.appendChild(keymapUi);
+</script>
+
+{% importcode "static/keymaps/title-screen-layout-layertest.js", "javascript" %}
---
+++ www/static/keymaps/title-screen-layout-layertest.js
@@ -0,0 +1,71 @@
+import { KeyboardModelTitleScreen } from "./title-screen-keyboard.js";
+import { KeymapLayout, KeymapKey, KeymapLayer } from "@keymapkit/ui";
+
+const welcome = [
+  `Welcome to the Layer Entry Key Test!`,
+  `This layout demonstrates layer entry key functionality.`,
+  `The purple key (P) is a layer entry key that switches to the second layer.`,
+  `Click on any key above to see information about it.`,
+];
+
+const secondLayerWelcome = [
+  `You're now on the second layer!`,
+  `This layer was accessed via the layer entry key from the main layer.`,
+  `The purple key (K) will take you back to the main layer.`,
+  `Click on any key above to see information about it.`,
+];
+
+const keyInfo = [
+  "Click another key, or click the same key again to close the info and go back to the welcome message.",
+];
+
+const layerEntryKeyInfo = [
+  "This is a layer entry key - it allows you to switch between layers.",
+  "Layer entry keys are highlighted in purple to show their special function.",
+  "Click this key to switch to the corresponding layer.",
+];
+
+/* A test layout with layer entry keys functionality
+ * Features two layers with keys that can switch between them
+ */
+export const KeymapTitleScreenLayoutLayerTest = new KeymapLayout({
+  displayName: "Title Screen Map (Layer Test)",
+  uniqueId: "title-screen-map-layertest",
+  model: KeyboardModelTitleScreen,
+  layers: [
+    KeymapLayer.fromKeyList({
+      displayName: "Main Layer",
+      shortName: "Main",
+      welcome: welcome,
+      keys: [
+        new KeymapKey({ name: "K", id: "title-1-1", info: keyInfo }),
+        new KeymapKey({ name: "E", id: "title-3-1", info: keyInfo }),
+        new KeymapKey({ name: "Y", id: "title-5-1", info: keyInfo }),
+        new KeymapKey({ name: "M", id: "title-7-1", info: keyInfo }),
+        new KeymapKey({ name: "A", id: "title-9-1", info: keyInfo }),
+        new KeymapKey({ name: "P", id: "title-11-1", info: keyInfo }),
+        new KeymapKey({ name: "K", id: "title-13-1", info: keyInfo }),
+        new KeymapKey({ name: "I", id: "title-15-1", info: keyInfo }),
+        new KeymapKey({ name: "T", id: "title-17-1", info: keyInfo }),
+      ]
+    }),
+    KeymapLayer.fromKeyList({
+      displayName: "Second Layer",
+      shortName: "Layer2",
+      welcome: secondLayerWelcome,
+      keys: [
+        new KeymapKey({ name: "K", id: "title-1-1", info: layerEntryKeyInfo }),
+        new KeymapKey({ name: "E", id: "title-3-1", info: keyInfo }),
+        new KeymapKey({ name: "Y", id: "title-5-1", info: keyInfo }),
+        new KeymapKey({ name: "M", id: "title-7-1", info: keyInfo }),
+        new KeymapKey({ name: "A", id: "title-9-1", info: keyInfo }),
+        new KeymapKey({ name: "P", id: "title-11-1", info: keyInfo }),
+        new KeymapKey({ name: "K", id: "title-13-1", info: keyInfo }),
+        new KeymapKey({ name: "I", id: "title-15-1", info: keyInfo }),
+        new KeymapKey({ name: "T", id: "title-17-1", info: keyInfo }),
+      ],
+      layerEntryKeys: ["title-1-1"], // K key (first one) enters this layer
+    }),
+  ],
+  guides: [],
+});
